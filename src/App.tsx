4eb574404cd7c201ec2500e0ad36b18f3de03720
Roman Chuchval <rchuchval@gmail.com>
2023-01-04 18:03:00 +0300

    map method microtasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Header} from "./Components/Header";
 import {Body} from "./Components/Body";
 import {Footer} from "./Components/Footer";
+import {NewComponent} from "./Components/NewComponent";
 
 function App() {
+
+    const students = [
+        {id: 1, name: "James", age: 8},
+        {id: 2, name: "Robert", age: 18},
+        {id: 3, name: "John", age: 28},
+        {id: 4, name: "Michael", age: 38},
+        {id: 5, name: "William", age: 48},
+        {id: 6, name: "David", age: 58},
+        {id: 7, name: "Richard", age: 68},
+        {id: 8, name: "Joseph", age: 78},
+        {id: 9, name: "Thomas", age: 88},
+        {id: 10, name: "Charles", age: 98},
+        {id: 11, name: "Christopher", age: 100},
+    ]
     return (
         <>
-            <Header titleForHeader={'NEW Header'}/>
-            <Body titleForBody={'NEW Body'}/>
-            <Footer titleForFooter={'NEW Footer'}/>
+            <NewComponent students={students}/>
+            {/*<Header titleForHeader={'NEW Header'}/>*/}
+            {/*<Body titleForBody={'NEW Body'}/>*/}
+            {/*<Footer titleForFooter={'NEW Footer'}/>*/}
         </>
     );
 }
---
+++ src/Components/NewComponent.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+type NewComponentType = {
+    //students:Array<StudentsInfoType>
+    students: StudentsInfoType[]
+}
+
+type StudentsInfoType = {
+    id: number;
+    name: string
+    age: number
+}
+
+export const NewComponent = (props: NewComponentType) => {
+
+    const topCars = [
+        {id: 1, manufacturer: 'BMW', model: 'm5cs'},
+        {id: 2, manufacturer: 'Mercedes', model: 'e63s'},
+        {id: 3, manufacturer: 'Audi', model: 'rs6'}
+    ]
+
+    return (
+        <div>
+            <ul>
+                {props.students.map((el, index) => {
+                    return (
+                        <li key={index}>name: {el.name}, age: {el.age}</li>
+                    )
+                })}
+            </ul>
+            <table>
+                <thead>
+                <tr>
+                    <th>Number</th>
+                    <th>Manufacturer</th>
+                    <th>Model</th>
+                </tr>
+                </thead>
+                <tbody>
+                {topCars.map((car, index) => {
+                    //debugger
+                    return (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{car.manufacturer}</td>
+                            <td>{car.model}</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
+    );
+};
